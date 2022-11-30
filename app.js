fa4250afb2186a8a307ce82adc5e6d148f8f0cd7
renuk21 <renukaganesh21@gmail.com>
2022-11-29 21:30:33 -0500

    A9

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
 
-const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/'
 const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
